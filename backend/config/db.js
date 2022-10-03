03b0ac982d28fe37ed8a1f626c2ea9d9b954a234
alishanmomin <mominalishan@gmail.com>
2022-10-03 17:42:26 +0500

    fixes

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,17 +3,20 @@ import mongoose from 'mongoose'
 
 
 
-const db = "mongodb+srv://momin123:momin123@cluster0.ipjqz.mongodb.net/mominshop?retryWrites=true&w=majority"
-const connectDB = async () => {
-    try {
+const db = "mongodb+srv://momin123:momin786@cluster0.ipjqz.mongodb.net/mominshop?retryWrites=true&w=majority"
+const connectDB = async () =>
+{
+    try
+    {
         const conn = await mongoose.connect(db, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
             useCreateIndex: true
         })
 
-        console.log(`MongoDb connected ${conn.connection.host}`)
-    } catch (error) {
+        console.log(`MongoDb connected ${ conn.connection.host }`)
+    } catch (error)
+    {
         console.log(error.message)
         process.exit(1)
     }
